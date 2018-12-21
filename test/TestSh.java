1786d66f238decf0a77697b3377ffc75a6d4a147
04756 <2351735698@qq.com>
2018-12-21 11:51:16 +0800

    构建平分类别树

--- test/TestSh.java
+++ test/TestSh.java
@@ -1,6 +1,7 @@
 import DAO.DAOBase;
 import DAO.ProductDAO;
 import po.Product;
+import po.TypeTree;
 
 import java.sql.*;
 
@@ -8,21 +9,24 @@ public class TestSh
 {
     public static void main(String[] args)
     {
-        try
-        {
-            Product pro = new Product(22, "2018女装毛衣", "详情介绍", "e:/", null, "1");
+//        try
+//        {
+           /* Product pro = new Product(22, "2018女装毛衣", "详情介绍", "e:/", null, "1");
             ProductDAO prodao = new ProductDAO();
             System.out.println(prodao.insert(pro) + "   " + pro.getPid());
             System.out.println(prodao.get("124").getTitle());
             pro.setTitle("更改后的裙子标题");
             prodao.update(pro);
             System.out.println(pro.getTitle());
-            System.out.println(prodao.delete(pro));
-        }
-        catch (SQLException e)
-        {
-            e.printStackTrace();
-        }
+            System.out.println(prodao.delete(pro));*/
+            TypeTree tt = new TypeTree();
+            tt = tt.createTree(2,4,"");
+            System.out.println(tt.getRangenex() + "   " + tt.getRangepre());
+//        }
+//        catch (SQLException e)
+//        {
+//            e.printStackTrace();
+//        }
 
     }
 }
