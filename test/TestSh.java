cecba95b0271b11c8a0c06c541234e755b14bdfa
andyzxq <1997112468182780@163.com>
2018-12-21 23:01:42 +0800

    订单商品表

--- test/TestSh.java
+++ test/TestSh.java
@@ -1,10 +1,8 @@
 import DAO.ordertableDAO;
+import DAO.ordertable_productDAO;
 import DAO.user_favorite_productDAO;
 import DAO.user_wanna_productDAO;
-import po.ordertable;
-import po.upPrimaryKey;
-import po.user_favorite_product;
-import po.user_wanna_product;
+import po.*;
 
 import java.sql.*;
 import java.util.List;
@@ -13,27 +11,30 @@ public class TestSh
 {
     public static void main(String[] args)
     {
-        ordertableDAO udao = new ordertableDAO();
-        ordertable o =new ordertable();
-        o.setOrderid("1");
-        o.setEstablishtime();
-        o.setAddressid("1");
-        List<ordertable> list = null;
+        ordertable_productDAO udao = new ordertable_productDAO();
+        ordertable_product o =new ordertable_product();
+        opPrimaryKey op = new opPrimaryKey();
+        op.setOrderid("1");
+        op.setPid("123");
+        o.setProNum(3);
+        o.setKey(op);
+        List<ordertable_product> list = null;
         try {
             udao.insert(o);
-            ordertable ufp2 = new  ordertable();
-            ufp2 = udao.get(o.getOrderid());
-            System.out.println(ufp2.getEstablishtime());
+            ordertable_product ufp2 = new  ordertable_product();
+            ufp2 = udao.get(o.getKey());
+            System.out.println(ufp2.getProNum());
+            ufp2.setProNum(5);
             try {
-                udao.update(ufp2.getOrderid(),null,ufp2.getAddressid());
+                udao.update(ufp2);
             } catch (Exception e) {
                 e.printStackTrace();
             }
-            list = udao.search("1",null,null);
-            udao.delete(ufp2.getOrderid());
+            list = udao.search("1",null,5);
+            udao.delete(ufp2.getProNum());
         } catch (SQLException e) {
             e.printStackTrace();
         }
-        System.out.println(list.size());
+        System.out.println(list.get(0).getProNum());
     }
 }
