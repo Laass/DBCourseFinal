af65c1293dae62d58bf1d5a0ba7a578594b53437
andyzxq <1997112468182780@163.com>
2018-12-21 21:30:11 +0800

    收藏夹

--- test/TestSh.java
+++ test/TestSh.java
@@ -1,6 +1,8 @@
+import DAO.user_favorite_productDAO;
 import DAO.user_wanna_productDAO;
+import po.upPrimaryKey;
+import po.user_favorite_product;
 import po.user_wanna_product;
-import po.uwpPrimaryKey;
 
 import java.sql.*;
 import java.util.List;
@@ -9,10 +11,20 @@ public class TestSh
 {
     public static void main(String[] args)
     {
-        user_wanna_productDAO udao = new user_wanna_productDAO();
-        List<user_wanna_product> list = null;
+        user_favorite_productDAO udao = new user_favorite_productDAO();
+        upPrimaryKey up = new upPrimaryKey();
+        user_favorite_product ufp = new  user_favorite_product();
+        up.setProductid("123");
+        up.setUserid("1");
+        ufp.setKey(up);
+        List<user_favorite_product> list = null;
         try {
-            list = udao.search("1","123",-1);
+            udao.insert(ufp);
+            user_favorite_product ufp2 = new  user_favorite_product();
+            ufp2 = udao.get(up);
+            System.out.println(ufp2.getKey().getUserid());
+            list = udao.search("1",null);
+            udao.delete(up);
         } catch (SQLException e) {
             e.printStackTrace();
         }
